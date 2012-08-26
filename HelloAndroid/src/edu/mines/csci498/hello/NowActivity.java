61d82ce88082bfeb1311abc4713c31e7197d2b1e
Bryan Wisdom <bwisdom@mymail.mines.edu>
2012-08-26 17:10:14 -0600

    Changed Functionality: App now updates timestamp upon button (screen) press.

--- HelloAndroid/src/edu/mines/csci498/hello/NowActivity.java
+++ HelloAndroid/src/edu/mines/csci498/hello/NowActivity.java
@@ -2,19 +2,34 @@ package edu.mines.csci498.hello;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
 
-public class NowActivity extends Activity {
-
+public class NowActivity extends Activity implements View.OnClickListener {
+  Button btn;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_now);
+        
+        btn=new Button(this);
+        btn.setOnClickListener(this);
+        updateTime();
+        setContentView(btn);
     }
-
+    
+    public void onClick(View view){
+    	updateTime();
+    }
+    
+    public void updateTime(){
+    	btn.setText(new Date().toString());
+    }
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_now, menu);
         return true;
     }
+    */
 }
