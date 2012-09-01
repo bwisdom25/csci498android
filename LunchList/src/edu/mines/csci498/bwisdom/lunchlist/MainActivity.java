a9bede7ac9623f62d29d2c75255b6cf4bb95b5a2
Bryan Wisdom <bwisdom@mymail.mines.edu>
2012-08-31 21:29:59 -0600

    Connected 'Save' button to save name and address to a Restaurant object

--- LunchList/src/edu/mines/csci498/bwisdom/lunchlist/MainActivity.java
+++ LunchList/src/edu/mines/csci498/bwisdom/lunchlist/MainActivity.java
@@ -3,18 +3,33 @@ package edu.mines.csci498.bwisdom.lunchlist;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	Restaurant r = new Restaurant();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button save = (Button) findViewById(R.id.save);
+
+		save.setOnClickListener(onSave);
+
+	}
+
+	private View.OnClickListener onSave = new View.OnClickListener() {
+		public void onClick(View v) {
+			EditText name = (EditText) findViewById(R.id.name);
+			EditText address = (EditText) findViewById(R.id.addr);
+
+			r.setName(name.getText().toString());
+			r.setAddress(address.getText().toString());
+
+		}
+	};
 }
