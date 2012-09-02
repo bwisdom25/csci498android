9b07ac34c684e708881a1fe2f25a6ed086b179d9
Bryan Wisdom <bwisdom@mymail.mines.edu>
2012-09-02 16:26:38 -0600

    Added AutoCompleteTextView for addresses, updated java activity to reflect changes and added example addresses for the white house, Graceland, and mall of America all of which are good grub-hauses!

--- LunchList/src/edu/mines/csci498/bwisdom/lunchlist/MainActivity.java
+++ LunchList/src/edu/mines/csci498/bwisdom/lunchlist/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -18,6 +19,13 @@ public class MainActivity extends Activity {
 	Restaurant r = new Restaurant();
 	List<Restaurant> model = new ArrayList<Restaurant>();
 	ArrayAdapter<Restaurant> adapter = null; 
+	ArrayAdapter<String> addressAdapter = null;
+	
+	
+	String[] addresses = { "1600 Pennsylvania Avenue, NW Washington, DC 20500" , 
+			"3765 Elvis Presley Blvd., Memphis, TN 38116.",
+			"60 East Broadway Bloomington, MN 55425"};
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,7 +36,13 @@ public class MainActivity extends Activity {
 
 		save.setOnClickListener(onSave);
 		
+		initiateSpinner();
 		
+		initiateAutoTextComplete();
+
+	}
+	
+	private void initiateSpinner(){
 		Spinner restSpinner = (Spinner) findViewById(R.id.restaurants);
 		
 		adapter = new ArrayAdapter<Restaurant>(this,
@@ -37,7 +51,17 @@ public class MainActivity extends Activity {
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		
 		restSpinner.setAdapter(adapter);
+	}
+	
+	private void initiateAutoTextComplete(){
+		int THRESHOLD = 2;
+		AutoCompleteTextView autoAddr = (AutoCompleteTextView) findViewById(R.id.addr);
 
+		addressAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_dropdown_item_1line, addresses);
+
+		autoAddr.setThreshold(THRESHOLD);
+		autoAddr.setAdapter(addressAdapter);
 	}
 
 	private View.OnClickListener onSave = new View.OnClickListener() {
