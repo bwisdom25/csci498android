07235bb0fd780e52521d5ba0a5f62ec2794c27c8
Bryan Wisdom <bwisdom@mymail.mines.edu>
2012-09-03 14:43:16 -0600

    Created specific RestaurantAdapter class (for customized row), created row.xml to specify the layout of custom row (included adding icon images in /res/drawable). Cleaned up drawable paths for screen resolution (only 1 drawable directory now). Updated logic in RestaurantApater class to create custom rows for Restaurant models

--- LunchList/src/edu/mines/csci498/bwisdom/lunchlist/MainActivity.java
+++ LunchList/src/edu/mines/csci498/bwisdom/lunchlist/MainActivity.java
@@ -5,18 +5,22 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	Restaurant r = new Restaurant();
 	List<Restaurant> model = new ArrayList<Restaurant>();
-	ArrayAdapter<Restaurant> adapter = null; 
+	RestaurantAdapter adapter = null; 
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -29,8 +33,7 @@ public class MainActivity extends Activity {
 		
 		ListView list = (ListView) findViewById(R.id.restaurants);
 		
-		adapter = new ArrayAdapter<Restaurant>(this,
-				android.R.layout.simple_expandable_list_item_1, model);
+		adapter = new RestaurantAdapter();
 		list.setAdapter(adapter);
 
 	}
@@ -60,4 +63,37 @@ public class MainActivity extends Activity {
 			adapter.add(r);  
 		}
 	};
+	
+	class RestaurantAdapter extends ArrayAdapter<Restaurant>{
+		RestaurantAdapter(){
+			super(MainActivity.this, android.R.layout.simple_list_item_1,model);
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent){
+			View row = convertView;
+			
+			if(row==null){
+				LayoutInflater inflater = getLayoutInflater();
+				
+				row = inflater.inflate(R.layout.row, null);
+			}
+			
+			Restaurant r = model.get(position);
+			
+			((TextView)row.findViewById(R.id.title)).setText(r.getName());
+			((TextView)row.findViewById(R.id.address)).setText(r.getAddress());
+			
+			ImageView icon = (ImageView) row.findViewById(R.id.icon);
+			
+			if(r.getType().equals("sit_down")){
+				icon.setImageResource(R.drawable.sit_down);
+			}else if(r.getType().equals("takeout")){
+				icon.setImageResource(R.drawable.takeout);
+			}else{
+				icon.setImageResource(R.drawable.delivery);
+			}
+			
+			return row;
+		}
+	}
 }
