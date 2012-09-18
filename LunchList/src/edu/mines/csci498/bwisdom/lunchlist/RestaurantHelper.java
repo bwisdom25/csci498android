14c38d79ce0909fed03615b3096c45a30cfa94dc
Bryan Wisdom <bwisdom@mymail.mines.edu>
2012-09-18 11:56:35 -0600

    Added onCreate to add a database with table 'restaurants' holding all valuable data, created insert function to insert a row into the DB, created cursor to return DB rows, added getters for each column , P.S. DB stands for Database . . . not Dumpster Baby

--- LunchList/src/edu/mines/csci498/bwisdom/lunchlist/RestaurantHelper.java
+++ LunchList/src/edu/mines/csci498/bwisdom/lunchlist/RestaurantHelper.java
@@ -1,6 +1,8 @@
 package edu.mines.csci498.bwisdom.lunchlist;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -13,11 +15,48 @@ class RestaurantHelper extends SQLiteOpenHelper {
 	}
 	
 	@Override
-	public void onCreate(SQLiteDatabase db){
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ " name TEXT, address TEXT, type TEXT, notes TEXT);");
 	}
 	
 	@Override 
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+		//Non-existent until 2nd schema version available 
+	}
+	
+	public void insert(String name, String address, String type, String notes){
 		
+		ContentValues cv = new ContentValues();
+		
+		cv.put("name", name);
+		cv.put("address", address);
+		cv.put("type", type);
+		cv.put("notes", notes);
+		
+		getWritableDatabase().insert("restaurants", "name", cv);
+	}
+	
+	public Cursor getAll() {
+		return (getReadableDatabase()
+				.rawQuery(
+						"SELECT _id, name, address, type, notes FROM restaurants ORDER BY NAME",
+						null));
+	}
+	
+	public String getName(Cursor c){
+		return(c.getString(1));
+	}
+	
+	public String getAddresss(Cursor c){
+		return(c.getString(2));
+	}
+	
+	public String getType(Cursor c){
+		return(c.getString(3));
+	}
+	
+	public String getNotes(Cursor c){
+		return(c.getString(4));
 	}
 }
