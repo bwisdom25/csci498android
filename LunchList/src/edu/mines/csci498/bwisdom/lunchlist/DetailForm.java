647647b0d624ff8f9d1d62642fdc4e345e058c57
Bryan Wisdom <bwisdom@mymail.mines.edu>
2012-10-02 09:06:54 -0600

    Fully Implemented RSS feed reader. Added FeedActivity to parse an RSS Feed and display titles in ListActivity type layout. Uses third-party apache jar 'android-rss.jar'

--- LunchList/src/edu/mines/csci498/bwisdom/lunchlist/DetailForm.java
+++ LunchList/src/edu/mines/csci498/bwisdom/lunchlist/DetailForm.java
@@ -1,14 +1,19 @@
 package edu.mines.csci498.bwisdom.lunchlist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 import android.util.Log;
 
 public class DetailForm extends Activity {
@@ -68,9 +73,36 @@ public class DetailForm extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		new MenuInflater(this).inflate(R.menu.details_option, menu);
-		
+		Log.d("DetailForm","Menu INflated");
 		return super.onCreateOptionsMenu(menu);
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d("DetailForm","MenuItem Selected");
+		if(item.getItemId() == R.id.menufeed) {
+			if( isNetworkAvailable() ) {
+				Intent i = new Intent(this, FeedActivity.class);
+				Log.d("DetailForm", "NetworkAvailalbe Starting intent");
+				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+				startActivity(i);
+			} else {
+				Toast.makeText(this, "Sorry, The Internets are not available", Toast.LENGTH_LONG).show();
+			}
+			
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+    private boolean isNetworkAvailable() {
+    	ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+    	
+    	NetworkInfo info = cm.getActiveNetworkInfo();
+    	
+    	return (info != null);
+    }
 	
 	private View.OnClickListener onSave = new View.OnClickListener() {
 		public void onClick(View v) {
