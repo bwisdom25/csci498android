5ad34a6448cf0372c1977112e53c57a06d00c5bf
Bryan Wisdom <bwisdom@mymail.mines.edu>
2012-10-30 16:04:53 -0600

    Created OnBootReciever Class to receive boot complete broadcast from the system. Added receiver to the manifast

---
+++ LunchList/src/edu/mines/csci498/bwisdom/lunchlist/OnBootReceiver.java
@@ -0,0 +1,15 @@
+package edu.mines.csci498.bwisdom.lunchlist;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
