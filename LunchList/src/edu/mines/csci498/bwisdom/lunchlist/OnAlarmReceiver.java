0d2e4f6ff97e2c69d04f40a2fc245cdb511b0d69
Bryan Wisdom <bwisdom@mymail.mines.edu>
2012-10-30 17:56:34 -0600

    Created Notification option for alarm. Updated preferences for checkbox, OnAlarmReceiver to accomidate user preference, and added a few strings to make it AWESOME!

--- LunchList/src/edu/mines/csci498/bwisdom/lunchlist/OnAlarmReceiver.java
+++ LunchList/src/edu/mines/csci498/bwisdom/lunchlist/OnAlarmReceiver.java
@@ -1,17 +1,44 @@
 package edu.mines.csci498.bwisdom.lunchlist;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class OnAlarmReceiver extends BroadcastReceiver{
+	
+	private static final int NOTIFY_ME_ID = 69;
 
 	@Override
 	public void onReceive(Context ctxt, Intent intent) {
-		Intent i = new Intent(ctxt, AlarmActivity.class);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctxt);
+		boolean useNotification = prefs.getBoolean("use_notification", true);
 		
-		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		
-		ctxt.startActivity(i);		
+		if(useNotification) {
+			NotificationManager mgr = (NotificationManager)ctxt.getSystemService(Context.NOTIFICATION_SERVICE);
+			
+			Notification note = new Notification(R.drawable.stat_notify_chat,ctxt.getString(R.string.alarmText),System.currentTimeMillis());
+			
+			PendingIntent i = PendingIntent.getActivity(ctxt, 0, new Intent(ctxt,AlarmActivity.class), 0);
+			
+			note.setLatestEventInfo(ctxt, ctxt.getString(R.string.app_name), ctxt.getString(R.string.alarmNote), i);
+			
+			note.flags|=Notification.FLAG_AUTO_CANCEL;
+			
+			mgr.notify(NOTIFY_ME_ID, note);
+			
+		} else {
+			
+			Intent i = new Intent(ctxt, AlarmActivity.class);
+			
+			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			
+			ctxt.startActivity(i);		
+			
+		}
 	}
 }
