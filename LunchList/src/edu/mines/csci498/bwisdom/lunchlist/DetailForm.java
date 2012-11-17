5ee994c76254d2c2867e5eb92e3c6d5ca8ee8c49
Bryan Wisdom <bwisdom@mymail.mines.edu>
2012-11-17 13:19:15 -0700

    Fixed Fatal error with Fragments, Now added DetailFragment Classes (I lied before oops!) App is correctly working now

--- LunchList/src/edu/mines/csci498/bwisdom/lunchlist/DetailForm.java
+++ LunchList/src/edu/mines/csci498/bwisdom/lunchlist/DetailForm.java
@@ -1,26 +1,9 @@
 package edu.mines.csci498.bwisdom.lunchlist;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.database.Cursor;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.util.Log;
+import android.support.v4.app.FragmentActivity;
 
-public class DetailForm extends Activity {
+public class DetailForm extends FragmentActivity {
 
 	@Override 
 	public void onCreate(Bundle savedInstanceState) {
